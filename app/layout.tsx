b43b7f7961bd683fa667f777ef4fbd9c1051e931
Bharat Taneja <113545688+Bharattaneja01@users.noreply.github.com>
2024-09-11 14:41:19 +0530

    Environment setup and navbar created

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import "./globals.css";
+import Navbar from "./components/Navbar/Navbar";
+// import ClientOnly from "./components/ClientOnly";
+import Modal from "./components/modals/Modal";
+import RegisterModal from "./components/modals/RegisterModal";
+import ToasterProvider from "./providers/ToasterProvider";
+import LoginModal from "./components/modals/LoginModal";
+
+const font = Nunito({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "AirBNB",
+  description: "AirBNB clone",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+            <ToasterProvider />
+            <LoginModal />
+            <RegisterModal />
+            <Navbar />
+        {children}
+        </body>
+    </html>
+  );
+}
